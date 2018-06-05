751d4e0bdbcc8577cea9053b8b9a4023b9731996
David Ayeke <ayekedavid@yahoo.com>
2018-06-05 17:54:56 -0500

    [General] First Iteration

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use std::io::prelude::*;
+use std::collections::HashMap;
+
+static ASCII_LOWER: [char; 26] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                                    's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+
+fn init(alphabet: &mut HashMap<char, bool>) {
+    for c in ASCII_LOWER.iter() {
+        alphabet.insert(c.clone(), false);
+    }
+}
+
+fn main() {
+    let mut alphabet: HashMap<char, bool> = HashMap::new();
+    init(&mut alphabet);
+
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).expect("ERROR");
+    for c in input.to_lowercase().chars() {
+        alphabet.insert(c.clone(), true);
+    }
+    let mut used = Vec::new();
+    let mut ignored = Vec::new();
+
+    for c in ASCII_LOWER.iter() {
+        if *alphabet.get(c).unwrap() {
+            used.push(c)
+        } else {
+            ignored.push(c)
+        }
+    }
+    println!("{:?}\n{:?}", used, ignored);
+}
